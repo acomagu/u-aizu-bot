0b9c93953daef2f454f6889614e62ca25bdc96d8
Yuki Ito <acomagu@gmail.com>
2016-09-19 22:42:33 +0900

    maybe fixed the problem

--- talk.go
+++ talk.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/acomagu/u-aizu-bot/types"
+	"fmt"
 )
 
 func talk(chatroom types.Chatroom) {
@@ -10,20 +11,32 @@ func talk(chatroom types.Chatroom) {
 			In:  make(chan types.Message),
 			Out: make(chan types.Message),
 		}
-		go topic(topicChatroom)
+		go loopTopic(topic, topicChatroom)
 		go sendMessageFromTopicChatroom(chatroom, topicChatroom)
 		go chainMessageFromChatroomToTopicChatroom(chatroom, topicChatroom)
 	}
 }
 
+func loopTopic(topic func(types.Chatroom), topicChatroom types.Chatroom) {
+	for {
+		topic(topicChatroom)
+	}
+}
+
 func chainMessageFromChatroomToTopicChatroom(chatroom types.Chatroom, topicChatroom types.Chatroom) {
 	for {
-		topicChatroom.In <- <-chatroom.In
+		// topicChatroom.In <- <-chatroom.In
+		tmp := <-chatroom.In
+		topicChatroom.In <- tmp
+		fmt.Println(tmp)
 	}
 }
 
 func sendMessageFromTopicChatroom(chatroom types.Chatroom, topicChatroom types.Chatroom) {
 	for {
-		chatroom.Out <- <-topicChatroom.Out
+		// chatroom.Out <- <-topicChatroom.Out
+		tmp := <-topicChatroom.Out
+		chatroom.Out <- tmp
+		fmt.Println(tmp)
 	}
 }
