a5698f5bc5c34943e82cdc923d283390b4042e3a
Yuki Ito <acomagu@gmail.com>
2016-09-19 22:34:46 +0900

    maybe fixed the problem

--- talk.go
+++ talk.go
@@ -5,9 +5,25 @@ import (
 )
 
 func talk(chatroom types.Chatroom) {
-	for {
-		for _, topic := range topics {
-			topic(chatroom)
+	for _, topic := range topics {
+		topicChatroom := types.Chatroom{
+			In:  make(chan types.Message),
+			Out: make(chan types.Message),
 		}
+		go topic(topicChatroom)
+		go sendMessageFromTopicChatroom(chatroom, topicChatroom)
+		go chainMessageFromChatroomToTopicChatroom(chatroom, topicChatroom)
+	}
+}
+
+func chainMessageFromChatroomToTopicChatroom(chatroom types.Chatroom, topicChatroom types.Chatroom) {
+	for {
+		topicChatroom.In <- <-chatroom.In
+	}
+}
+
+func sendMessageFromTopicChatroom(chatroom types.Chatroom, topicChatroom types.Chatroom) {
+	for {
+		chatroom.Out <- <-topicChatroom.Out
 	}
 }
